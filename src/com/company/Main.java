72c2c60c6c22c513b0e8bf0c5f688ad7eebea8f7
extgamonzon <ext_gamonzon@mercadolibre.com>
2022-01-04 22:06:40 -0300

    adding goodbye greeting

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("goodbye git, I prefer to version with zip and google docs :(");
     }
 }
